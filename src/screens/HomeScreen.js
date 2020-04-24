b57a65d640826a777cb94a925c36b718f911d42d
Niels Koster <n.koster@portavita.eu>
2020-04-25 00:11:56 +0200

    simple button

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,22 @@
-import React from "react";
-import { Text, StyleSheet } from "react-native";
+import React from 'react'
+import { Text, StyleSheet, View, Button } from 'react-native'
 
-const HomeScreen = () => {
-return <Text style={styles.text}>hey you!</Text>;
-};
+const HomeScreen = _ => {
+  return (
+    <View>
+      <Text style={styles.text}>hey you!</Text>
+      <Button
+        onPress={_ => console.log('Pressed')}
+        title='The Component'
+      />
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30
   }
-});
+})
 
-export default HomeScreen;
+export default HomeScreen
