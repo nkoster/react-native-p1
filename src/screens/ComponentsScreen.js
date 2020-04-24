491e0e432b892a7cbb3a9658b6fbc25f54ded081
Niels Koster <n.koster@portavita.eu>
2020-04-24 23:08:00 +0200

    text + style

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,14 +1,23 @@
 import React from 'react'
 
-import {Text, StyleSheet} from 'react-native'
+import {Text, StyleSheet, View} from 'react-native'
 
 const ComponentsScreen = _ => {
-    return <Text style={styles.textStyle}>I am The Components Screen</Text>
+    const greeting = 'N3L'
+    return (
+        <View>
+            <Text style={styles.textStyle}>I am The Component</Text>
+            <Text style={styles.textSecond}>{greeting}</Text>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
     textStyle: {
-        fontSize: 100
+        fontSize: 30
+    },
+    textSecond: {
+        fontSize: 15
     }
 })
 
