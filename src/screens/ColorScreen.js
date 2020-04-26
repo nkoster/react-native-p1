93389c2a6a4a8cd548ad18cb17ff080c9eb17ee0
Niels Koster <n.koster@portavita.eu>
2020-04-26 22:25:12 +0200

    Initial, color array + useState()

---
+++ src/screens/ColorScreen.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { View, StyleSheet, Button, FlatList } from 'react-native'
+
+const ColorScreen = _ => {
+    const [colors, setColor] = useState([])
+    console.log(colors)
+    return (
+        <View>
+            <Button title='add color' onPress={_ => {
+                setColor([...colors, renderRgb()])
+            }} />
+            <FlatList
+                keyExtractor={item => item}
+                data={colors}
+                renderItem={ ({item}) => {
+                return <View style={{ height: 100, width: 100, backgroundColor: item}} />
+                }}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+const renderRgb = _ => `rgb(${
+    Math.floor(Math.random() * 256)
+},${
+    Math.floor(Math.random() * 256)
+},${
+    Math.floor(Math.random() * 256)
+})`
+
+export default ColorScreen
