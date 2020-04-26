7f7292ed229e3472d67fbbdb20ef792e09703f45
Niels Koster <n.koster@portavita.eu>
2020-04-26 13:49:42 +0200

    Initial

---
+++ src/components/ImageDetail.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+
+const ImageDetail = _ => {
+    return (
+        <View>
+            <Text>ImageDetail</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default ImageDetail
