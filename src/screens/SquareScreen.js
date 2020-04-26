d032dbc936e880a15dd23bd376aa441644dd4388
Niels Koster <n.koster@portavita.eu>
2020-04-27 01:15:21 +0200

    use setColor()

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -7,23 +7,37 @@ const SquareScreen = _ => {
     const [green, setGreen] = useState(Math.floor(Math.random() * 256))
     const [blue, setBlue] = useState(Math.floor(Math.random() * 256))
     const factor = 10
-
+    const setColor = (color, change) => {
+        switch(color) {
+            case 'red':
+                red + change > 255 || red + change < 0 ? null : setRed(red + change)
+                return
+            case 'green':
+                green + change > 255 || green + change < 0 ? null : setGreen(green + change)
+                return
+            case 'blue':
+                blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change)
+                return
+            default:
+                return
+        }
+    }
     return (
         <View>
             <ColorChanger
                 color='red'
-                onMore={_ => red < (255 - factor) && setRed(red + factor)}
-                onLess={_ => red > factor && setRed(red - factor)}
+                onMore={_ => setColor('red', factor)}
+                onLess={_ => setColor('red', -1 * factor)}
             />
             <ColorChanger
                 color='green'
-                onMore={_ => green < (255 - factor) && setGreen(green + factor)}
-                onLess={_ => green > factor && setGreen(green - factor)}
+                onMore={_ => setColor('green', factor)}
+                onLess={_ => setColor('green', -1 * factor)}
             />
             <ColorChanger
                 color='blue'
-                onMore={_ => blue < (255 - factor) && setBlue(blue + factor)}
-                onLess={_ => blue > factor && setBlue(blue - factor)}
+                onMore={_ => setColor('blue', factor)}
+                onLess={_ => setColor('blue', -1 * factor)}
             />
             <View
                 style={{height: 150, width: 300, backgroundColor: `rgb(${red},${green},${blue})`}}
