11c936f4a284128d65e77ce43dc621fd41e5b8af
Niels Koster <n.koster@portavita.eu>
2020-04-28 23:31:13 +0200

    Use "community convention".

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -5,19 +5,19 @@ import ColorChanger from '../components/ColorChanger'
 const factor = 10
 
 const reducer = (state, action) => {
-    switch (action.colorToChange) {
+    switch (action.type) {
         case 'red':
-            return state.red + action.amount > 255 || state.red + action.amount < 0
+            return state.red + action.payload > 255 || state.red + action.payload < 0
                 ? state
-                : { ...state, red: state.red + action.amount }
+                : { ...state, red: state.red + action.payload }
         case 'green':
-            return state.green + action.amount > 255 || state.green + action.amount < 0
+            return state.green + action.payload > 255 || state.green + action.payload < 0
             ? state
-            : { ...state, green: state.green + action.amount }
+            : { ...state, green: state.green + action.payload }
         case 'blue':
-            return state.blue + action.amount > 255 || state.blue + action.amount < 0
+            return state.blue + action.payload > 255 || state.blue + action.payload < 0
             ? state
-            : { ...state, blue: state.blue + action.amount }
+            : { ...state, blue: state.blue + action.payload }
         default:
             return state
     }
@@ -34,18 +34,18 @@ const SquareScreen = _ => {
         <View>
             <ColorChanger
                 color='red'
-                onMore={_ => dispatch({ colorToChange: 'red', amount: factor })}
-                onLess={_ => dispatch({ colorToChange: 'red', amount: -1 * factor })}
+                onMore={_ => dispatch({ type: 'red', payload: factor })}
+                onLess={_ => dispatch({ type: 'red', payload: -1 * factor })}
             />
             <ColorChanger
                 color='green'
-                onMore={_ => dispatch({ colorToChange: 'green', amount: factor })}
-                onLess={_ => dispatch({ colorToChange: 'green', amount: -1 * factor })}
+                onMore={_ => dispatch({ type: 'green', payload: factor })}
+                onLess={_ => dispatch({ type: 'green', payload: -1 * factor })}
             />
             <ColorChanger
                 color='blue'
-                onMore={_ => dispatch({ colorToChange: 'blue', amount: factor })}
-                onLess={_ => dispatch({ colorToChange: 'blue', amount: -1 * factor })}
+                onMore={_ => dispatch({ type: 'blue', payload: factor })}
+                onLess={_ => dispatch({ type: 'blue', payload: -1 * factor })}
             />
             <View
                 style={{height: 150, width: 300, backgroundColor: `rgb(${red},${green},${blue})`}}
