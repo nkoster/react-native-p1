fe7c0ad3c59a0a504d4e095322408c93a920d0c2
Niels Koster <n.koster@portavita.eu>
2020-04-29 00:20:59 +0200

    Change to useReducer() exercise, my solution

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,21 +1,17 @@
-import React, { useState} from 'react'
-import { View, StyleSheet, Text, Button } from 'react-native'
+import React, { useReducer } from 'react'
+import { View, Text, Button } from 'react-native'
+
+const reducer = (_, action) => action.payload
 
 const CounterScreen = _ => {
-    const [counter, setCounter] = useState(0)
+    const [state, dispatch] = useReducer(reducer, 0)
     return (
         <View>
-            <Button title='Increase' onPress={_ => {
-                setCounter(counter + 1)
-            }} />
-            <Button title='Decrease' onPress={_ => {
-                setCounter(counter - 1)
-            }} />
-            <Text>Counter Value: {counter}</Text>
+            <Button title='Increase' onPress={_ => dispatch({ payload: state + 1 })} />
+            <Button title='Decrease' onPress={_ => dispatch({ payload: state - 1 })} />
+            <Text>Counter Value: {state}</Text>
         </View>
     )
 }
-
-const styles = StyleSheet.create({})
 
 export default CounterScreen
