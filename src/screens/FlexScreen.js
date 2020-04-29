615cd45863f962455ba2d111c0254700c1d3b65c
Niels Koster <n.koster@portavita.eu>
2020-04-30 01:07:52 +0200

    test flex in children

--- src/screens/FlexScreen.js
+++ src/screens/FlexScreen.js
@@ -4,11 +4,9 @@ import { View, Text, StyleSheet } from 'react-native'
 const FlexScreen = _ => {
     return (
         <View style={styles.viewStyle} >
-            <Text style={styles.textStyle} >Flex I</Text>
-            <Text style={styles.textStyle} >Flex II</Text>
-            <Text style={styles.textStyle} >Flex III</Text>
-            <Text style={styles.textStyle} >Flex IIII</Text>
-            <Text style={styles.textStyle} >Flex IIIII</Text>
+            <Text style={styles.textOneStyle} >Flex I</Text>
+            <Text style={styles.textTwoStyle} >Flex II</Text>
+            <Text style={styles.textThreeStyle} >Flex III</Text>
         </View>
     )
 }
@@ -17,14 +15,31 @@ const styles = StyleSheet.create({
     viewStyle: {
         borderWidth: 1,
         borderColor: 'lightgray',
-        height: 300,
-        justifyContent: 'center'
+        flexDirection: 'row',
+        height: 300
     },
-    textStyle: {
+    textOneStyle: {
         borderWidth: 1,
         borderColor: 'green',
         margin: 2,
         padding: 5,
+        flex: 1,
+        borderRadius: 15
+    },
+    textTwoStyle: {
+        borderWidth: 1,
+        borderColor: 'green',
+        margin: 2,
+        padding: 5,
+        flex: 1,
+        borderRadius: 15
+    },
+    textThreeStyle: {
+        borderWidth: 1,
+        borderColor: 'green',
+        margin: 2,
+        padding: 5,
+        flex: 1,
         borderRadius: 15
     }
 })
