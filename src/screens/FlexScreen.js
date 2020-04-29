e8223fae78154f9238301ad0d9de39df97774f57
Niels Koster <n.koster@portavita.eu>
2020-04-29 23:11:53 +0200

    alignItems example

--- src/screens/FlexScreen.js
+++ src/screens/FlexScreen.js
@@ -4,7 +4,11 @@ import { View, Text, StyleSheet } from 'react-native'
 const FlexScreen = _ => {
     return (
         <View style={styles.viewStyle} >
-            <Text style={styles.textStyle} >Flex Screen</Text>
+            <Text style={styles.textStyle} >Flex I</Text>
+            <Text style={styles.textStyle} >Flex II</Text>
+            <Text style={styles.textStyle} >Flex III</Text>
+            <Text style={styles.textStyle} >Flex IIII</Text>
+            <Text style={styles.textStyle} >Flex IIIII</Text>
         </View>
     )
 }
@@ -13,7 +17,7 @@ const styles = StyleSheet.create({
     viewStyle: {
         borderWidth: 1,
         borderColor: 'lightgray',
-        borderRadius: 50
+        alignItems: 'flex-end'
     },
     textStyle: {
         borderWidth: 1,
