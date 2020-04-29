07c41aae3e7df89592d1726ac05725107be644f8
Niels Koster <n.koster@portavita.eu>
2020-04-29 22:31:23 +0200

    Initial, BoxScreen layout

---
+++ src/screens/BoxScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { TextInput } from 'react-native-gesture-handler'
+
+const BoxScreen = _ => {
+    return (
+        <View>
+            <Text>Box Screen</Text>
+        </View>
+    )
+}
+
+export default BoxScreen
