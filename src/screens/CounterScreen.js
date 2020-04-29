5f28032069c77354e319b648bec1d97e1acb37e8
Niels Koster <n.koster@portavita.eu>
2020-04-29 11:03:02 +0200

    best practice

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,15 +1,20 @@
 import React, { useReducer } from 'react'
 import { View, Text, Button } from 'react-native'
 
-const reducer = (_, action) => action.payload
-
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'incr': return {...state, count: state.count + action.payload}
+        case 'decr': return {...state, count: state.count - action.payload}
+        default: return state
+    }
+}
 const CounterScreen = _ => {
-    const [state, dispatch] = useReducer(reducer, 0)
+    const [state, dispatch] = useReducer(reducer, { count: 0 })
     return (
         <View>
-            <Button title='Increase' onPress={_ => dispatch({ payload: state + 1 })} />
-            <Button title='Decrease' onPress={_ => dispatch({ payload: state - 1 })} />
-            <Text>Counter Value: {state}</Text>
+            <Button title='Increase' onPress={_ => dispatch({ type: 'incr', payload: 1 })} />
+            <Button title='Decrease' onPress={_ => dispatch({ type: 'decr', payload: 1 })} />
+            <Text>Counter Value: {state.count}</Text>
         </View>
     )
 }
