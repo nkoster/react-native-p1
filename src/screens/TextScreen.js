57adf6f4f0f5d92c6d1b64e432349ea01a0b685b
Niels Koster <n.koster@portavita.eu>
2020-04-29 11:50:31 +0200

    Initial

---
+++ src/screens/TextScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Button, Text } from 'react-native'
+
+const TextScreen = _ => {
+    return (
+        <View>
+            <Text>Text Screen</Text>
+        </View>
+    )
+}
+
+export default TextScreen
