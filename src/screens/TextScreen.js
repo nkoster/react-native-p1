c27dc54f4be3cb5bdeabf48d6b05ef7dab5dd06f
Niels Koster <n.koster@portavita.eu>
2020-04-29 21:55:57 +0200

    add state

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,15 +1,18 @@
-import React from 'react'
-import { View, TextInput, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, TextInput, StyleSheet, Text } from 'react-native'
 
 const TextScreen = _ => {
+    const [name, setName] = useState('')
     return (
         <View>
             <TextInput
                 style={styles.input}
                 autoCapitalize='none'
                 autoCorrect={false}
-                value='apekop'
+                value={name}
+                onChangeText={inputText => setName(inputText)}
             />
+            <Text>{name}</Text>
         </View>
     )
 }
