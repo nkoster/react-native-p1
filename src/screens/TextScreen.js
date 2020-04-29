ac13c0fb97ccae7d22ad51a748fca69848b493d3
Niels Koster <n.koster@portavita.eu>
2020-04-29 21:37:49 +0200

    TextScreen starter

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,12 +1,27 @@
 import React from 'react'
-import { View, Button, Text } from 'react-native'
+import { View, TextInput, StyleSheet } from 'react-native'
 
 const TextScreen = _ => {
     return (
         <View>
-            <Text>Text Screen</Text>
+            <TextInput
+                style={styles.input}
+                autoCapitalize='none'
+                autoCorrect={false}
+                value='apekop'
+            />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    input: {
+        margin: 15,
+        borderColor: 'black',
+        borderWidth: 1,
+        backgroundColor: 'green',
+        color: 'white'
+    }
+})
 
 export default TextScreen
