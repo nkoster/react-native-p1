689b163072b14e05ec882e029f95ed0883065264
Niels Koster <n.koster@portavita.eu>
2020-04-29 22:52:58 +0200

    Cool styling stuff

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -1,13 +1,27 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import { TextInput } from 'react-native-gesture-handler'
 
 const BoxScreen = _ => {
     return (
-        <View>
-            <Text>Box Screen</Text>
+        <View style={styles.viewStyle} >
+            <Text style={styles.textStyle} >Box Screen</Text>
         </View>
     )
 }
 
+const styles = StyleSheet.create({
+    viewStyle: {
+        borderWidth: 1,
+        borderColor: 'lightgray',
+        borderRadius: 50
+    },
+    textStyle: {
+        borderWidth: 1,
+        borderColor: 'green',
+        margin: 2,
+        padding: 5,
+        borderRadius: 15
+    }
+})
 export default BoxScreen
